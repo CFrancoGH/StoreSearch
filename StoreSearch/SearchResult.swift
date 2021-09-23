41ac7ffc0b680a9d8f0a3c3be6fcfc4f0b340e5e
Christopher Franco <email.cfranco@gmail.com>
2021-09-23 10:21:34 -0400

    a

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,15 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Christopher Franco on 9/22/21.
+//
+
+import Foundation
+
+class SearchResult {
+    
+    var name = ""
+    var artistName = ""
+    
+}
